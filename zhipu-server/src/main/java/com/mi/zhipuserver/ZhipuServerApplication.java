da939aee5650b0601e9c0d9ac939ebc1697e16c9
睿智 <3372563469@qq.com>
2025-04-17 08:36:20 +0800

    add -AI-repository初始化     -zhipu-server的初始化demo

---
+++ zhipu-server/src/main/java/com/mi/zhipuserver/ZhipuServerApplication.java
@@ -0,0 +1,13 @@
+package com.mi.zhipuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhipuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhipuServerApplication.class, args);
+    }
+
+}
